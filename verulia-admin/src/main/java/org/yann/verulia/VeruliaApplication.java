debf3d47d96734e5698940f651bce09ad84f33e5
Yann <1319542051@qq.com>
2025-12-15 12:12:32 +0800

    init

---
+++ verulia-admin/src/main/java/org/yann/verulia/VeruliaApplication.java
@@ -0,0 +1,18 @@
+package org.yann.verulia;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Yann
+ * @date 2025/12/12 17:03
+ */
+@SpringBootApplication
+public class VeruliaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeruliaApplication.class, args);
+    }
+}
---
+++ verulia-modules/verulia-system/src/main/java/org/yann/verulia/system/package-info.java
@@ -0,0 +1,1 @@
+package org.yann.verulia.system;
