d2ac914073db2949ea7e47acf258940f948cc786
Yann <1319542051@qq.com>
2025-12-25 23:54:15 +0800

    init

--- verulia-framework/verulia-framework-web/src/main/java/org/yann/verulia/framework/web/config/JacksonConfig.java
+++ verulia-framework/verulia-framework-web/src/main/java/org/yann/verulia/framework/web/config/JacksonConfig.java
@@ -3,6 +3,8 @@ package org.yann.verulia.framework.web.config;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import tools.jackson.core.json.JsonReadFeature;
+import tools.jackson.databind.SerializationFeature;
 import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.databind.module.SimpleModule;
 import tools.jackson.databind.ser.std.ToStringSerializer;
@@ -18,20 +20,24 @@ public class JacksonConfig {
 
     @Bean
     @Primary
-    public JsonMapper jsonMapper() {
+    public JsonMapper jacksonJsonMapper() {
         // 创建一个模块，专门负责 Long -> String 的转换
         SimpleModule longToStringModule = new SimpleModule();
-
-        // 1. 针对包装类 Long
+        // 针对包装类 Long
         longToStringModule.addSerializer(Long.class, ToStringSerializer.instance);
-        // 2. 针对基本类型 long
+        // 针对基本类型 long
         longToStringModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-
-        // 构建 JsonMapper
+        // 强制使用 Builder 模式创建
         return JsonMapper.builder()
                 .addModule(longToStringModule)
-                // 施主如果有其他配置（如时间格式）也可以连在后面写
-                // .enable(SerializationFeature.INDENT_OUTPUT)
+                // 配置特性：不再是 set/enable，而是 configure 或 enable/disable
+                // 处理空 Bean 不报错
+                .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                // 开启格式化输出
+                .enable(SerializationFeature.INDENT_OUTPUT)
+                // 针对 JSON 解析的细粒度控制 (新 API)
+                .enable(JsonReadFeature.ALLOW_JAVA_COMMENTS)
+                // 构建不可变的 Mapper 实例
                 .build();
     }
 }
