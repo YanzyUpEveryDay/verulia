d9559fd91ad34df851c496aaf0f4a484dd20c8fa
Yann <1319542051@qq.com>
2025-12-26 13:45:21 +0800

    add: jackson扩展配置

--- verulia-framework/verulia-framework-web/src/main/java/org/yann/verulia/framework/web/config/JacksonConfig.java
+++ verulia-framework/verulia-framework-web/src/main/java/org/yann/verulia/framework/web/config/JacksonConfig.java
@@ -1,13 +1,19 @@
 package org.yann.verulia.framework.web.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import tools.jackson.core.json.JsonReadFeature;
-import tools.jackson.databind.SerializationFeature;
-import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.ext.javatime.deser.LocalDateTimeDeserializer;
+import tools.jackson.databind.ext.javatime.ser.LocalDateTimeSerializer;
 import tools.jackson.databind.module.SimpleModule;
 import tools.jackson.databind.ser.std.ToStringSerializer;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Jackson 配置类
@@ -15,29 +21,48 @@ import tools.jackson.databind.ser.std.ToStringSerializer;
  *
  * @author Yann
  */
+@Slf4j
 @AutoConfiguration
 public class JacksonConfig {
 
+    @Value("${spring.jackson.date-format: yyyy-MM-dd HH:mm:ss}")
+    private String dateFormat;
+
+    /**
+     * jackson配置
+     */
     @Bean
-    @Primary
-    public JsonMapper jacksonJsonMapper() {
-        // 创建一个模块，专门负责 Long -> String 的转换
-        SimpleModule longToStringModule = new SimpleModule();
-        // 针对包装类 Long
-        longToStringModule.addSerializer(Long.class, ToStringSerializer.instance);
-        // 针对基本类型 long
-        longToStringModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-        // 强制使用 Builder 模式创建
-        return JsonMapper.builder()
-                .addModule(longToStringModule)
-                // 配置特性：不再是 set/enable，而是 configure 或 enable/disable
-                // 处理空 Bean 不报错
-                .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-                // 开启格式化输出
-                .enable(SerializationFeature.INDENT_OUTPUT)
-                // 针对 JSON 解析的细粒度控制 (新 API)
-                .enable(JsonReadFeature.ALLOW_JAVA_COMMENTS)
-                // 构建不可变的 Mapper 实例
-                .build();
+    public JsonMapperBuilderCustomizer moduleCustomizer() {
+        return builder -> {
+            builder.addModule(numberToStringModule());
+            builder.addModule(dateTimeToStringModule());
+            log.info("===>> 初始化 Jackson 配置.");
+        };
+    }
+
+    /**
+     * 数字 转 String
+     */
+    public SimpleModule numberToStringModule() {
+        SimpleModule module = new SimpleModule();
+        // long
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        // integer
+        module.addSerializer(BigInteger.class, ToStringSerializer.instance);
+        // decimal
+        module.addSerializer(BigDecimal.class, ToStringSerializer.instance);
+        return module;
+    }
+
+    /**
+     * localDateTime 转 String
+     */
+    public SimpleModule dateTimeToStringModule() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(formatter));
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(formatter));
+        return module;
     }
 }
